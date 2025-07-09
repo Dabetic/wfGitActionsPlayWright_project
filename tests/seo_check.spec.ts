f10c93353b1d0ca3f5e1cce0db15b6c27b794b1e
Milos <milos.dabetic@gmail.com>
2025-07-09 14:05:01 +0200

    Save results to txt file added

--- tests/seo_check.spec.ts
+++ tests/seo_check.spec.ts
@@ -1,12 +1,15 @@
 import {test, expect} from '@playwright/test'
 
+import fs from 'fs';
+
+
 
 test.beforeEach(async ({page}) => {
 
     const link: string = process.env.TEST_LINK || ''
     console.log(link)
     await page.goto(link)
-    
+
 })
 
 
@@ -21,16 +24,30 @@ test('checking meta title', async({page}) => {
     const content = await metaTitle.textContent()
 
     console.log(content)
+
+    let rezultat: string = ""
+
+    if (content && content.trim() !== '') {
+        rezultat = "✅ Sve je ok"
+        console.log(rezultat);
+      } else {
+        rezultat = "❌ Nedostaje meta title"
+        console.log(rezultat);
+      }
     
+      console.log('Rezultati nakon' + rezultat)
     
     expect(content).not.toBeNull();
     expect(content?.trim()).not.toBe('')
     
+
+    fs.writeFileSync('backend/test-results/playwright-result.txt', `Rezultat testa: ${rezultat}`);
+
     
     })
 
 
-test('checking meta description', async({page}) => {
+test.skip('checking meta description', async({page}) => {
 
 
 const metaDescription = page.locator('meta[name="description"]')
@@ -39,6 +56,14 @@ await expect(metaDescription).toBeAttached()
 
 
 const content = await metaDescription.getAttribute('content');
+
+ if (content && content.trim() !== '') {
+        let akoJeProso = "Sve je ok"
+        console.log(akoJeProso);
+      } else {
+        let akoNije = "Nedostaje meta title"
+        console.log(akoNije);
+      }
 
 
 expect(content).not.toBeNull();
